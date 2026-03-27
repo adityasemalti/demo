04654743d6728b6b8860adbb15182b5de4128de5
Aditya Semalti <154693138+adityasemalti@users.noreply.github.com>
2026-03-27 11:22:13 +0530

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,18 +1,11 @@
 import mongoose from 'mongoose'
 
-export const connectDb = async () => {
-  try {
-    if (mongoose.connection.readyState >= 1) return;
-
-    await mongoose.connect(
-      "mongodb+srv://adityasemalti535_db_user:Aditya%402003@cluster0.wcg7dza.mongodb.net",
-      {
-        dbName: "User",
-      }
-    );
-
-    console.log("Database connected");
-  } catch (error) {
-    console.log(error);
-  }
-};
+export const connectDb = async()=>{
+    try {
+        await mongoose.connect("mongodb+srv://adityasemalti535_db_user:Aditya%402003@cluster0.wcg7dza.mongodb.net",{
+            dbName: "User"
+        }).then(()=>console.log("Database connected"))
+    } catch (error) {
+        console.log(error)
+    }
+}
