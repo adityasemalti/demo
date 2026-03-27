9f143ec45b1b82bc2262de38c28a7270f7149552
Aditya Semalti <154693138+adityasemalti@users.noreply.github.com>
2026-03-27 10:51:03 +0530

    Update db.js

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connectDb = async()=>{
     try {
-        await mongoose.connect("mongodb+srv://adityasemalti535:Aditya%402003@cluster0.ta1t5q6.mongodb.net/",{
+        await mongoose.connect("mongodb+srv://adityasemalti535_db_user:Aditya@2003@cluster0.wcg7dza.mongodb.net",{
             dbName: "User"
         }).then(()=>console.log("Database connected"))
     } catch (error) {
